8ca14509fb2e9d13a1d025351c12594252cb5e92
Samira Rodrigues <samira.silva@zup.com.br>
2024-07-17 15:17:57 -0300

    add exercicio6

---
+++ src/exercicio6.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class exercicio6 {
+    public static void main(String[] args) {
+        int base = scan("Escreva a base: ").nextInt();
+        int exponent = scan("Escreva o expoente: ").nextInt();
+        int result = 1;
+
+        for (int i = 0; i < exponent; i++) {
+            result *= base;
+        }
+
+        System.out.println(base + "^" + exponent + " = " + result);
+
+    }
+
+    public static Scanner scan(String message) {
+        System.out.println(message);
+        return new Scanner(System.in);
+    }
+
+}
