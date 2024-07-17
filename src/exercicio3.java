263506df5e9866c71ec40ce483341a8faa8a0acd
Samira Rodrigues <samira.silva@zup.com.br>
2024-07-17 15:13:44 -0300

    add exercicio3

---
+++ src/exercicio3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class exercicio3 {
+    public static void main(String[] args) {
+        int number = scanner("Escreva um numero: ").nextInt();
+        String textNumber = Integer.toString(number); // metodo dentro da classe Integer que converte um inteiro para String
+
+        System.out.println(textNumber.length());
+    }
+    public static Scanner scanner(String message){
+        System.out.println(message);
+        return new Scanner(System.in);
+    }
+}
