be89c531fc7bbcaa950394ea961337e833bb135e
Samira Rodrigues <samira.silva@zup.com.br>
2024-07-17 15:12:16 -0300

    add exercicio2

---
+++ src/exercicio2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class exercicio2 {
+    public static void main(String[] args) {
+
+        int n = scanner("Escreva um numero: ").nextInt();
+        int fat = n; // inicializa o fatorial como n
+
+        while (n > 1){
+            fat *= (n - 1); // fatorial vai multiplicar (n - 1)
+            n--; // n vai decrementando até chegar em 1 parar
+        }
+        System.out.println("Fatorial de N: "+fat);
+    }
+    public static Scanner scanner(String message){
+        System.out.println(message);
+        return new Scanner(System.in);
+    }
+}
